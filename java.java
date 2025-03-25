dc49a8b6a6757d9e922541eec2db629a4060d1a4
Taha Kadhum <tahakadhum1@gmail.com>
2025-03-25 19:22:16 +0000

    reformatting code to make it cleaner.

--- java.java
+++ java.java
@@ -1,4 +1,4 @@
-
+// Purpose: This program calculates the monthly payment for a loan based on the principal amount, deposit, annual interest rate, and term of the loan.
 import java.text.NumberFormat;
 import java.util.*;
 
@@ -6,68 +6,56 @@ public class java {
 
     public static void main(String[] args) {
 
-        int mInYear = 12;
-        int percent = 100;
+        
         int principal = 0;
         int deposit = 0;
         double annualInterestRate = 0;
         int years = 0;
+        double payment = 0;
         
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-        System.out.print("Enter the principal amount for your loan: ");
-        principal = scanner.nextInt();
-        if (1000000 > principal && principal > 0) {
-            break;
-        } else {
-            System.out.println("Please enter a valid principal amount.");
-        }
-        }
+        principal = (int)readNumber("Enter your principal loan: ", 1000, 1000000);
+        deposit = (int) readNumber("Enter your deposit amount: ", 0, principal);
+        annualInterestRate = (float) readNumber("Enter the annual interest rate for your loan: ", 0, 100);
+        years = (int) readNumber("Enter the term of your loan (years): ", 0, 30);
 
-        while (true) {
-        System.out.println("Enter your deposit amount: ");
-        deposit = scanner.nextInt();
-        if (deposit < principal && deposit > 0) {
-            break;
-            } else {
-                System.out.println("Please enter a valid deposit.");
-            } 
-        }
-
-        while (true) {
-        System.out.print("Enter the annual interest rate for your loan: ");
-        annualInterestRate = scanner.nextDouble();
-        if (annualInterestRate > 0 && annualInterestRate < 100) {
-            break;
-        } else {
-            System.out.println("Please enter a valid interest rate.");
-        }
-        }
-        
-        while (true) { 
-            System.out.println("Enter the term of your loan in years: ");
-            years = scanner.nextInt();
-            if (years > 0 && years <= 30)
-            break;
-            else if (years > 30) {
-                System.out.println("We do not offer loans longer than 30 years.");
-          } else {
-            System.out.println("Please enter a valid term.");
-          }
-        }
-
-        double monthlyInterestRate = annualInterestRate / percent / mInYear;
-        int months = years * mInYear;
-
-        double mathPow = Math.pow(1 + monthlyInterestRate, months);
-        double monthlyPayment = (principal - deposit) * monthlyInterestRate / (1 - 1 / mathPow);
+        payment = calculateMortgage(principal, deposit, annualInterestRate, years);
 
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
-        String payment = formatter.format(monthlyPayment);
+        String monthlyPayment = formatter.format(payment);
 
-        System.out.println("Your monthly payment will be: " + payment);
+        System.out.println("Your monthly payment will be: " + monthlyPayment + " over a term of " + years + " years.");
     }
 
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if (value >= min && value <= max) {
+                break;
+            } else {
+                System.out.println("Enter a value between " + min + " and " + max);
+            }
+            }
+            return value;
+    }
+
+    public static double calculateMortgage(
+        int principal,
+        int deposit,
+        double annualInterestRate,
+        int years) {
+            final int mInYear = 12;
+            final int percent = 100;
+            double monthlyInterestRate = annualInterestRate / percent / mInYear;
+            int months = years * mInYear;
+            double mathPow = Math.pow(1 + monthlyInterestRate, months);
+            double payment = (principal - deposit) * monthlyInterestRate / (1 - 1 / mathPow);
+
+            return payment;
+    }
 
 }
