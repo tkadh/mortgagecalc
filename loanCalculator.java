95a6312b865e0cf5a6f60a8cd52a0bba06bb938d
Taha Kadhum <tahakadhum1@gmail.com>
2025-04-01 22:56:45 +0100

    fixed static referencing issues

--- loanCalculator.java
+++ loanCalculator.java
@@ -4,57 +4,56 @@ import java.util.*;
 
 public class LoanCalculator {
 
-    final int mInYear = 12;
-    final int percent = 100;
-    public static void main(String[] args) {
-        // Read user inputs
-        int principal = (int) readNumber("Enter your principal loan: ", 1000, 1000000);
-        int deposit = (int) readNumber("Enter your deposit amount: ", 0, principal);
-        float annualInterestRate = (float) readNumber("Enter the annual interest rate for your loan: ", 0, 100);
-        int years = (int) readNumber("Enter the term of your loan (years): ", 1, 30);
-        // Calculate the monthly payment
-        double payment = calculateMortgage(principal, deposit, annualInterestRate, years);
-        // Format the payment as currency
-        NumberFormat formatter = NumberFormat.getCurrencyInstance();
-        String monthlyPayment = formatter.format(payment);
-
-        System.out.println("Your monthly payment will be: " + monthlyPayment + " over a term of " + years + " years.");    
-    }
-
-    // Method to read user input within specified range
-    public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
-
-        double value;
-        NumberFormat formatter = NumberFormat.getInstance();
-
-        while (true) {
-            System.out.print(prompt);
-            value = scanner.nextDouble();
-            if (value >= min && value <= max) {
-                break;
-            } else {
-                System.out.println("Enter a value between " + formatter.format((int)min) + " and " + formatter.format((int)max));
-            }
-            }
-            
-            return value;
-    }
-
-    // Method to calculate the monthly payment
-    public static double calculateMortgage(
-        int principal,
-        int deposit,
-        double annualInterestRate,
-        int years) {
-            double monthlyInterestRate = annualInterestRate / percent / mInYear;
-            int months = years * mInYear;
+        final static int mInYear = 12;
+        final static int percent = 100;
+        public static void main(String[] args) {
+            // Read user inputs
+            int principal = (int) readNumber("Enter your principal loan: ", 1000, 1000000);
+            int deposit = (int) readNumber("Enter your deposit amount: ", 0, principal);
+            float annualInterestRate = (float) readNumber("Enter the annual interest rate for your loan: ", 0, 100);
+            int years = (int) readNumber("Enter the term of your loan (years): ", 1, 30);
+            // Calculate the monthly payment
+            double payment = calculateMortgage(principal, deposit, annualInterestRate, years);
+            // Format the payment as currency
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+            String monthlyPayment = formatter.format(payment);
+    
+            System.out.println("Your monthly payment will be: " + monthlyPayment + " over a term of " + years + " years.");    
+        }
+    
+        // Method to read user input within specified range
+        public static double readNumber(String prompt, double min, double max) {
+            Scanner scanner = new Scanner(System.in);
+    
+            double value;
+            NumberFormat formatter = NumberFormat.getInstance();
+    
+            while (true) {
+                System.out.print(prompt);
+                value = scanner.nextDouble();
+                if (value >= min && value <= max) {
+                    break;
+                } else {
+                    System.out.println("Enter a value between " + formatter.format((int)min) + " and " + formatter.format((int)max));
+                }
+                }
+                
+                return value;
+        }
+    
+        // Method to calculate the monthly payment
+        public static double calculateMortgage(
+            int principal,
+            int deposit,
+            double annualInterestRate,
+            int years) {
+                double monthlyInterestRate = annualInterestRate / percent / mInYear;
+                int months = years * mInYear;
             double mathPow = Math.pow(1 + monthlyInterestRate, months);
             double payment = (principal - deposit) * monthlyInterestRate / (1 - 1 / mathPow);
 
             return payment;
     }
-}
 
     public static double calculateBalance(int principal,
     int deposit,
@@ -70,3 +69,4 @@ public class LoanCalculator {
             return balance;
             
     }
+}
